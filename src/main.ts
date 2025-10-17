8765d142f56795531a03704f527f5bf4b9ee17e9
Раков Олег Денисович <olegrakovic323@gmail.com>
2025-10-17 10:23:36 +0300

    feat: Add backup mode

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { Bot } from '@maxhub/max-bot-api'
 import dotenv from 'dotenv'
-dotenv.config()
+dotenv.config({
+    override: false,
+})
 
 import BetterContext from '@/BetterContext';
 import stateMiddleware from './middleware/state';
@@ -10,44 +12,73 @@ import format from './utils/format';
 import lang from '@/strings/ru.json'
 import { reset_state } from './state/state_managing';
 
-// Создайте экземпляр класса Bot и передайте ему токен 
 const bot = new Bot(process.env["BOT_TOKEN"]!, {contextType: BetterContext});
 
-bot.use(stateMiddleware)
+async function run_backup() {
+    bot.on([
+        "message_callback",
+        "message_created",
+        "message_removed",
+        "message_edited",
+        "bot_added",
+        "bot_removed",
+        "user_added",
+        "user_removed",
+        "bot_started",
+        "chat_title_changed",
+        "message_construction_request",
+        "message_constructed",
+        "message_chat_created"
+    ], async (ctx) => {
+        ctx.reply(lang.STUB_MESSAGE)
+    })
+    let bot_promise = bot.start();
+    console.log("BOT READY IN BACKUP MODE")
 
-// bot.command('test', (ctx) => ctx.reply('Доброasdasd!'));
+    await bot_promise;
+}
 
-bot.on('bot_started', async (ctx) => {
-    await reset_state(ctx.user!.user_id.toString())
+async function run() {
+    bot.use(stateMiddleware)
 
-    await ctx.reply(format(lang.WELCOME_MESSAGE))
-})
+    bot.on('bot_started', async (ctx) => {
+        await reset_state(ctx.user!.user_id.toString())
+        await ctx.reply(format(lang.WELCOME_MESSAGE))
+    })
 
-bot.on([
-    "message_callback",
-    "message_created",
-    "message_removed",
-    "message_edited",
-    "bot_added",
-    "bot_removed",
-    "user_added",
-    "user_removed",
-    "bot_started",
-    "chat_title_changed",
-    "message_construction_request",
-    "message_constructed",
-    "message_chat_created"
-], async (ctx) => {
-    if (!ctx.currentState) {
-        await ctx.reply(format(lang.CURRENT_STATE_ERROR, {"ctx": ctx}))
-        return
-    }
-    await ctx.reply(`${JSON.stringify(ctx.currentState.metadata)}`)
-    setTimeout(async () => {
-        await processState(ctx, ctx.currentState!)
-    }, 200)
-})
+    bot.on([
+        "message_callback",
+        "message_created",
+        "message_removed",
+        "message_edited",
+        "bot_added",
+        "bot_removed",
+        "user_added",
+        "user_removed",
+        "bot_started",
+        "chat_title_changed",
+        "message_construction_request",
+        "message_constructed",
+        "message_chat_created"
+    ], async (ctx) => {
+        if (!ctx.currentState) {
+            await ctx.reply(format(lang.CURRENT_STATE_ERROR, {"ctx": ctx}))
+            return
+        }
+        await ctx.reply(`${JSON.stringify(ctx.currentState.metadata)}`)
+        setTimeout(async () => {
+            await processState(ctx, ctx.currentState!)
+        }, 200)
+    })
 
-// Теперь можно запустить бота, чтобы он подключился к серверам MAX и ждал обновлений
-bot.start();
-console.log("BOT READY")
+    let bot_promise = bot.start();
+    console.log("BOT READY");
+
+    await bot_promise;
+}
+
+if (process.env.BOT_STATE === "backup") {
+    run_backup()
+} else {
+    run()
+}
