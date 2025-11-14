060c0115d5d89db0ba92f8fde03a8e5c119d6d11
Раков Олег Денисович <olegrakovic323@gmail.com>
2025-11-14 11:53:47 +0300

    feat: Number selector now can have buttons before and after "cancel"

--- src/state/states/utils/NumberSelector.ts
+++ src/state/states/utils/NumberSelector.ts
@@ -1,62 +1,70 @@
-import format from "@/utils/format";
-import IState from "../../IState";
-import { get_state_from_id } from "../../state_managing";
-import lang from "@/strings/ru.json"
-import { Keyboard } from "@maxhub/max-bot-api";
-import BetterContext from "@/BetterContext";
+import format from '@/utils/format'
+import IState from '../../IState'
+import { get_state_from_id } from '../../state_managing'
+import lang from '@/strings/ru.json'
+import { Keyboard } from '@maxhub/max-bot-api'
+import BetterContext from '@/BetterContext'
 
 interface InitParams {
-    nextState: IState<any>,
-    nextStateFunctionName: string,
-    nextStateCancelFunctionName?: string,
-    prompt?: string,
+    nextState: IState<any>
+    nextStateFunctionName: string
+    nextStateCancelFunctionName?: string
+    prompt?: string
     cancel_text?: string
-    min?: number,
+    min?: number
     max?: number
+    before_additional_buttons?: {payload: string, text: string}[]
+    after_additional_buttons?: {payload: string, text: string}[]
 }
 
 interface Metadata {
     selector: {
-        next_state_id: string,
-        callback_name: string,
-        cancel_callback_name?: string,
-        min: number,
-        max: number,
-        text: string,
-        cancel_text: string,
+        next_state_id: string
+        callback_name: string
+        cancel_callback_name?: string
+        min?: number
+        max?: number
+        text: string
+        cancel_text: string
 
-        waiting: boolean,
+        waiting: boolean
+
+        before_additional_buttons: {payload: string, text: string}[]
+        after_additional_buttons: {payload: string, text: string}[]
     }
 }
 
 enum payloads {
-    CANCEL = "cancel"
+    CANCEL = 'cancel',
 }
-function getRangeText(selector_min: number, selector_max: number) {
-    if (selector_min != -Infinity && selector_max != Infinity) {
+function getRangeText(selector_min?: number, selector_max?: number) {
+    if (selector_min && selector_max) {
         return format(lang.NUMBER_SELECTOR.RANGE_MIN_MAX_DEFINED, {
-            "selector_min": selector_min,
-            "selector_max": selector_max
+            selector_min: selector_min,
+            selector_max: selector_max,
         })
-    } else if (selector_min != -Infinity) {
+    } else if (selector_min) {
         return format(lang.NUMBER_SELECTOR.RANGE_MIN_DEFINED, {
-            "selector_min": selector_min
+            selector_min: selector_min,
         })
-    } else if (selector_max != Infinity) {
+    } else if (selector_max) {
         return format(lang.NUMBER_SELECTOR.RANGE_MAX_DEFINED, {
-            "selector_max": selector_max
+            selector_max: selector_max,
         })
     }
-    return ""
+    return ''
 }
 
 export let NumberSelector: IState<InitParams> = {
-    state_id: "number_selector",
-    active_on: "all",
+    state_id: 'number_selector',
+    active_on: 'all',
     async process_state(ctx: BetterContext<Metadata>) {
-        const next_state = get_state_from_id(ctx.metadata.selector.next_state_id)!
+        const next_state = get_state_from_id(
+            ctx.metadata.selector.next_state_id
+        )!
         const state_callback_func = ctx.metadata.selector.callback_name!
-        const state_cancel_callback_func = ctx.metadata.selector.cancel_callback_name
+        const state_cancel_callback_func =
+            ctx.metadata.selector.cancel_callback_name
 
         const selector_min = ctx.metadata.selector.min
         const selector_max = ctx.metadata.selector.max
@@ -64,13 +72,22 @@ export let NumberSelector: IState<InitParams> = {
         const selector_text = ctx.metadata.selector.text
         const cancel_text = ctx.metadata.selector.cancel_text
 
+        const before_additional_buttons = ctx.metadata.selector.before_additional_buttons
+        const after_additional_buttons = ctx.metadata.selector.after_additional_buttons
+
+        const all_buttons = before_additional_buttons.concat(after_additional_buttons)
+
+        if (all_buttons.find((val) => val.payload === ctx.callback?.payload)) {
+            return await next_state.process_state(ctx)
+        }
+
         if (ctx.callback?.payload === payloads.CANCEL) {
             ctx.callback.payload = undefined
 
             if (state_cancel_callback_func) {
                 return await next_state[state_cancel_callback_func](ctx)
             }
-            return next_state
+            return await next_state.process_state(ctx)
         }
 
         const waiting = ctx.metadata.selector.waiting
@@ -78,8 +95,13 @@ export let NumberSelector: IState<InitParams> = {
         const input_value = Number(ctx.message?.body.text)
 
         if (waiting && input_value) {
-            if (input_value < selector_min || input_value > selector_max) {
-                ctx.reply(format(lang.NUMBER_SELECTOR.WRONG_MESSAGE, {range: getRangeText(selector_min, selector_max)}))
+            if (input_value < (selector_min ?? Infinity) || input_value > (selector_max ?? Infinity)) {
+                await ctx.reply(
+                    format(lang.NUMBER_SELECTOR.WRONG_MESSAGE, {
+                        range: getRangeText(selector_min, selector_max),
+                    }),
+                    { format: 'markdown' }
+                )
                 return NumberSelector
             }
 
@@ -89,16 +111,30 @@ export let NumberSelector: IState<InitParams> = {
         }
 
         if (waiting) {
-            ctx.reply(format(lang.NUMBER_SELECTOR.WRONG_MESSAGE, { range: getRangeText(selector_min, selector_max) }))
+            await ctx.reply(
+                format(lang.NUMBER_SELECTOR.WRONG_MESSAGE, {
+                    range: getRangeText(selector_min, selector_max),
+                }),
+                {
+                    format: 'markdown',
+                }
+            )
             return NumberSelector
         }
 
-        ctx.reply(selector_text, {
+        await ctx.reply(selector_text, {
             attachments: [
                 Keyboard.inlineKeyboard([
-                    [Keyboard.button.callback(cancel_text, payloads.CANCEL)]
-                ])
-            ]
+                    ...before_additional_buttons.map((val) => {
+                        return [Keyboard.button.callback(val.text, val.payload)]
+                    }),
+                    [Keyboard.button.callback(cancel_text, payloads.CANCEL)],
+                    ...after_additional_buttons.map((val) => {
+                        return [Keyboard.button.callback(val.text, val.payload)]
+                    })
+                ]),
+            ],
+            format: 'markdown',
         })
 
         ctx.metadata.selector.waiting = true
@@ -106,11 +142,17 @@ export let NumberSelector: IState<InitParams> = {
         return NumberSelector
     },
 
-    async init(ctx: BetterContext<Metadata>, params: InitParams): Promise<IState<InitParams>> {
-        const selector_min = params.min ?? -Infinity
-        const selector_max = params.max ?? Infinity
+    async init(
+        ctx: BetterContext<Metadata>,
+        params: InitParams
+    ): Promise<IState<InitParams>> {
+        // Infinity... can't be serialized to json...
+        const selector_min = params.min
+        const selector_max = params.max
 
-        let default_selector_text = format(lang.NUMBER_SELECTOR.DEFAULT, { "range": getRangeText(selector_min, selector_max) })
+        let default_selector_text = format(lang.NUMBER_SELECTOR.DEFAULT, {
+            range: getRangeText(selector_min, selector_max),
+        })
 
         ctx.metadata.selector = {
             next_state_id: params.nextState.state_id,
@@ -121,6 +163,8 @@ export let NumberSelector: IState<InitParams> = {
             text: params.prompt ?? default_selector_text,
             cancel_text: params.cancel_text ?? lang.CANCEL,
             waiting: false,
+            before_additional_buttons: params.before_additional_buttons ?? [],
+            after_additional_buttons: params.after_additional_buttons ?? [],
         }
 
         return NumberSelector
@@ -128,5 +172,5 @@ export let NumberSelector: IState<InitParams> = {
 
     async clear(ctx) {
         delete ctx.metadata.selector
-    }
+    },
 }
